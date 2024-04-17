f1274ad9e628238ef6736db7218c972cf244db38
Anna <hhermione@yandex.ru>
2024-04-17 10:13:59 +0500

    Добавила зависимость JUnit, сетап для базовых тестов

---
+++ src/test/java/HippodromeTest.java
@@ -0,0 +1,23 @@
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HippodromeTest {
+
+    @BeforeClass
+    public static void setup() {
+    }
+
+    @Test
+    void getHorses() {
+    }
+
+    @Test
+    void move() {
+    }
+
+    @Test
+    void getWinner() {
+    }
+}
---
+++ src/test/java/HorseTest.java
@@ -0,0 +1,27 @@
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HorseTest {
+
+    @BeforeClass
+    public static void setup() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getSpeed() {
+    }
+
+    @Test
+    void getDistance() {
+    }
+
+    @Test
+    void move() {
+    }
+}
